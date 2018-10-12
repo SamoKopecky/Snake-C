573efedae037d5db8ba63815f1ec2cf330c27b02
Samuel Kopecký <xkopec52@vutbr.cz>
2018-10-12 18:02:37 +0200

    added functions for playground

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <time.h>
 
-void createPlayground(char playground[][]) {
-    for (int x = 0; x < sizeof(playground/)/sizeof(playground[0]); ++x) {
-        for (int y = 0; y < sizeof(playground[0]); ++y) {
+int playgroundX = 10;
+int playgroundY = 10;
+
+void createPlayground(char playground[][10]) {
+    for (int x = 0; x < playgroundX; ++x) {
+        for (int y = 0; y < playgroundY; ++y) {
             playground[x][y] = '*';
         }
     }
 }
 
-void printPlayground(char playground) {
-    for (int x = 0; x < sizeof(playground)/sizeof(playground[0]); ++x) {
+void printPlayground(char playground[][10]) {
+    for (int x = 0; x < playgroundX; ++x) {
         printf("\n");
-        for (int y = 0; y < sizeof(playground[0]); ++y) {
+        for (int y = 0; y < playgroundY; ++y) {
             printf("%c", playground[x][y]);
+
         }
     }
 }
@@ -23,10 +27,6 @@ int main() {
     createPlayground(playground);
     printPlayground(playground);
     //unsigned long currentTime = time(NULL);
-
-
-
-
     //do {
 
     //}while (true);

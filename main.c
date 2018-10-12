b7ce73784cb991050214d61e6f5d69eb6157ab99
Samuel Kopecký <xkopec52@vutbr.cz>
2018-10-12 17:43:51 +0200

    test

--- main.c
+++ main.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
+#include <time.h>
 
-int main() {
-    char playground[15][15];
-
-    for (int x = 0; x < sizeof(playground)/sizeof(playground[0]); ++x) {
+void createPlayground(char playground[][]) {
+    for (int x = 0; x < sizeof(playground/)/sizeof(playground[0]); ++x) {
         for (int y = 0; y < sizeof(playground[0]); ++y) {
             playground[x][y] = '*';
         }
     }
+}
 
+void printPlayground(char playground) {
     for (int x = 0; x < sizeof(playground)/sizeof(playground[0]); ++x) {
         printf("\n");
         for (int y = 0; y < sizeof(playground[0]); ++y) {
             printf("%c", playground[x][y]);
         }
     }
+}
+
+int main() {
+    char playground[10][10];
+    createPlayground(playground);
+    printPlayground(playground);
+    //unsigned long currentTime = time(NULL);
+
+
+
+
+    //do {
+
+    //}while (true);
+
+
     return 0;
 }

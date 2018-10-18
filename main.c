02d10ec32cbfe0fa5bdafeab893890bb994dc630
Samuel Kopecký <xkopec52@vutbr.cz>
2018-10-18 16:30:44 +0200

    One block snake movement

--- main.c
+++ main.c
@@ -3,15 +3,8 @@
 #include <time.h>
 
 char playground[25][50];
-int snakeLenght = 2;
-
-void createPlayground() {
-    for (int x = 0; x < sizeof(playground)/ sizeof(playground[0]); ++x) {
-        for (int y = 0; y < sizeof(playground[0]) / sizeof(playground[0][0]); ++y) {
-            playground[x][y] = '*';
-        }
-    }
-}
+int snakeCords[2] = {2, 2};
+int delay = 1;
 
 void printPlayground() {
     for (int x = 0; x < sizeof(playground)/ sizeof(playground[0]); ++x) {
@@ -24,27 +17,31 @@ void printPlayground() {
 }
 
 void updateSnake() {
-    //snakeY++;
-    //snakeX++;
+    snakeCords[0]++;
+    snakeCords[1]++;
 
 }
 
 bool checkForSnakeCoordinates(int x, int y) {
-    return false;
-};
+    if(x == snakeCords[0] && y == snakeCords[1]) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 void updatePlayground() {
     for (int x = 0; x < sizeof(playground)/ sizeof(playground[0]); ++x) {
         for (int y = 0; y < sizeof(playground[0]); ++y) {
             if (checkForSnakeCoordinates(x, y)) {
                 playground[x][y] = 'O';
+            } else {
+                playground[x][y] = ' ';
             }
             if (x == 0 || x == sizeof(playground) / sizeof(playground[0]) - 1 || y == 0 || y == sizeof(playground[0]) - 1) {
                 playground[x][y] = '*';
             }
-            else {
-                playground[x][y] = ' ';
-            }
+
         }
     }
 }
@@ -54,14 +51,14 @@ int main() {
     updatePlayground();
     printPlayground();
 
-    unsigned long currentTime = time(NULL) + 1;
+    unsigned long currentTime = time(NULL) + delay;
 
     while(true) {
         if (currentTime == time(NULL)) {
             updateSnake();
             updatePlayground();
             printPlayground();
-            currentTime = time(NULL) + 1;
+            currentTime = time(NULL) + delay;
         }
     }
 
